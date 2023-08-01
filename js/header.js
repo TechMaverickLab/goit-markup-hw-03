ac7cdc37e6d6f2f331c1c803a4b8a7a1e89ba739
rlypovskyi <121307948+rlypovskyi@users.noreply.github.com>
2023-08-01 09:22:04 -0400

    1

--- js/header.js
+++ js/header.js
@@ -1,15 +1,3 @@
-//   <!--  Скріпт для встановлення автоматичної висоти фіксованого елемента Heder та відступу padding для тексту  при зміні висоти контенту-->
-
-
-    // console.log(document.querySelector(".page-header").getBoundingClientRect().height);
-
-    // const { height: pageHeaderHeight } = document
-    //   .querySelector(".page-header")
-    //   .getBoundingClientRect();
-
-    // document.body.style.paddingTop = '${pageHeaderHeight}px';
-
-
 
 window.onscroll = () => changePageHeaderBackground();
 
